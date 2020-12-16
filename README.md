b5f523cc3ee10f0f6cb25093a7781fc5f78a0b49
will <65295334+willmade@users.noreply.github.com>
2020-12-16 18:07:17 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Author William H.
+
+### Desription:
+
+This is a simple example of a function that can read a file in a stream of data, then print the data to the console.
+
+# To Run Program
+
+Run `$yarn start`
---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+ Author William H.
+ Desription: This is a simple example of
+    a function that can read a file in a stream of data,
+    then print the data to the console.
+*/
+
+// Must require module for working with the filesystem
+const fs = require('fs')
+
+
+let streamfunc = (source, output=process.stdout) => {
+ const stream = fs.createReadStream(__dirname +'/'+ source)
+ stream.pipe(output)
+}
+
+// Just enter the file name
+// The 'source' arg must be in ./ 
+streamfunc('data.json')
+
+
